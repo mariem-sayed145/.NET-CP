c2ab502b966daf89c1b8301dbec4543d19a71cc3
Mariam Sayed <mariemsayedr33@gmail.com>
2026-09-07 11:03:56 +0300

    Add caching to GetAllProductsQueryHandler
    Implemented caching for product retrieval using Redis.

--- ECommerce.Application/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
+++ ECommerce.Application/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using ECommerce.Application.Interfaces.Repositories;
+using ECommerce.Application.Interfaces.Repositories;
+using ECommerce.Application.Interfaces.Services;
 using ECommerce.Application.Products.DTOs;
 using MediatR;
 
@@ -7,21 +8,46 @@ namespace ECommerce.Application.Products.Queries.GetAllProducts;
 public sealed class GetAllProductsQueryHandler
     : IRequestHandler<GetAllProductsQuery, IReadOnlyList<ProductResponse>>
 {
+    private const string CacheKey = "products:all";
+
     private readonly IProductRepository _productRepository;
+    private readonly ICacheService _cache;
 
     public GetAllProductsQueryHandler(
-        IProductRepository productRepository)
+        IProductRepository productRepository,
+        ICacheService cache)
     {
         _productRepository = productRepository;
+        _cache = cache;
     }
 
     public async Task<IReadOnlyList<ProductResponse>> Handle(
         GetAllProductsQuery request,
         CancellationToken cancellationToken)
     {
+        // 1. Try to get products from Redis
+        var cachedProducts =
+            await _cache.GetAsync<List<ProductResponse>>(CacheKey);
+
+        // 2. Cache Hit
+        if (cachedProducts is not null)
+        {
+            return cachedProducts;
+        }
+
+        // 3. Cache Miss → Get data from database
         var products = await _productRepository.GetAllAsync(
             cancellationToken);
 
-        return products.ToResponseList();
+        var response = products.ToResponseList();
+
+        // 4. Store the result in Redis
+        await _cache.SetAsync(
+            CacheKey,
+            response,
+            TimeSpan.FromMinutes(10));
+
+        // 5. Return data
+        return response;
     }
-} 
+}
