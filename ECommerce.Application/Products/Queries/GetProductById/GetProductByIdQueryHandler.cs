39467bbf92b0ea0034438030c41a753dad00121a
Mariam Sayed <mariemsayedr33@gmail.com>
2026-09-07 11:04:29 +0300

    Add caching to GetProductByIdQueryHandler
    Implement caching for product retrieval using Redis.

--- ECommerce.Application/Products/Queries/GetProductById/GetProductByIdQueryHandler.cs
+++ ECommerce.Application/Products/Queries/GetProductById/GetProductByIdQueryHandler.cs
@@ -1,5 +1,6 @@
-﻿using ECommerce.Application.Common.Exceptions;
+using ECommerce.Application.Common.Exceptions;
 using ECommerce.Application.Interfaces.Repositories;
+using ECommerce.Application.Interfaces.Services;
 using ECommerce.Application.Products.DTOs;
 using ECommerce.Domain.Entities;
 using MediatR;
@@ -10,17 +11,33 @@ public sealed class GetProductByIdQueryHandler
     : IRequestHandler<GetProductByIdQuery, ProductResponse>
 {
     private readonly IProductRepository _productRepository;
+    private readonly ICacheService _cache;
 
     public GetProductByIdQueryHandler(
-        IProductRepository productRepository)
+        IProductRepository productRepository,
+        ICacheService cache)
     {
         _productRepository = productRepository;
+        _cache = cache;
     }
 
     public async Task<ProductResponse> Handle(
         GetProductByIdQuery request,
         CancellationToken cancellationToken)
     {
+        var cacheKey = $"product:{request.Id}";
+
+        // 1. Try Redis
+        var cachedProduct =
+            await _cache.GetAsync<ProductResponse>(cacheKey);
+
+        // 2. Cache Hit
+        if (cachedProduct is not null)
+        {
+            return cachedProduct;
+        }
+
+        // 3. Cache Miss → Database
         var product = await _productRepository.GetByIdAsync(
             request.Id,
             cancellationToken);
@@ -32,6 +49,15 @@ public sealed class GetProductByIdQueryHandler
                 request.Id);
         }
 
-        return product.ToResponse();
+        var response = product.ToResponse();
+
+        // 4. Save in Redis
+        await _cache.SetAsync(
+            cacheKey,
+            response,
+            TimeSpan.FromMinutes(10));
+
+        // 5. Return
+        return response;
     }
-} 
+}
